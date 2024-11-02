e9414aa5b1ed8ee4d112b7bd7cb8a8782b365386
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-11-02 16:18:59 -0700

    Fix bug in Genre menu

--- lib/cli.py
+++ lib/cli.py
@@ -13,45 +13,48 @@ from helpers import (
 def main():
     main_menu()
 
-#**********************************  Genre Menu Methods ******************************************
+#**********************************  Main Menu  ******************************************
 
 genres = list_genres()
    
 def main_menu():    
-    def new_genre():
-        create_genre()
-        main_menu()
-
+    
     functions = [("Create Genre", new_genre), ("Genre List", genres_menu_choices), ("Choose Genre", selecting_genre), ("Delete", delete_genre)]
-
     choice = ''
-    
-    print_line()
-    print("Welcome to the World's Top Music Store\n")
-    print("      ROCK STARS OF THE WORLDS       \n\n\n\n")
-    print("Please select music genre or add new one:\n")
-    
-    #print Genre menu 
-    for index in range(len(functions)):
-        print(f"{index + 1}: {functions[index][0]}")
 
-    print("\nPress 'e' to exit the program.")
+    #print Genre menu     
+    starting_lines_for_genre_methods()      
+    print("             MAIN MENU       \n\n")
+    for index in range(len(functions)):
+        print(f"{index + 1}: {functions[index][0]}")    
+    
+    print("\nPress 'e' to exit the program.")    
     print_line()  
 
     #selecting Genre menu
     while choice != 'e':        
         choice = input("> ")  
-        for index in range(len(functions)):
+        for index in range(len(functions) + 1):
             if choice == str(index):
+                print(f"index: {index}")
+                print(functions[index-1][0])
                 functions[index - 1][1]()
                 break
         if choice == "e":
             exit_program()
         else:
-            print("Invalid choice")  
+            print("Invalid choice") 
 
+#Create new Genre
+def new_genre():
+        create_genre()
+        main_menu()
+        # print_genre_list()
+
+#Genre list
 def genres_menu_choices():    
     print_genre_list()
+    print("\nPlease select music genre or go back to add new one!")
     ending_lines_for_genre_methods()
     choice = input("> ")
     while choice != 'e' or choice != 'm':
@@ -65,29 +68,35 @@ def genres_menu_choices():
             print("Invalid choice")
         choice = input("> ")
 
+#Genre delete
+# def 
+
+#Genre helping methods
+
 def selecting_genre():
-    # selected = {}
     print_genre_list()
     choice = input("> ")    
     for index in range(len(genres)):
         if choice == str(index + 1):
             print(f"Selected genre: {genres[index].name}")
             break
-
+    print("\nPlease select genre to see bands of that genre!")
     ending_lines_for_genre_methods()
 
 def print_genre_list():
     starting_lines_for_genre_methods()
+    print("             GENRE LIST       \n")
     for index, genre in enumerate(genres):
         print(f"{index + 1}: {genre.name}")
 
 def starting_lines_for_genre_methods():
-    print_line()
-    print("Please select genre to see bands of that genre:\n")
+    print_line() 
+    print("Welcome to the World's Top Music Store")
+    print("      ROCK STARS OF THE WORLDS       \n\n")   
 
 def ending_lines_for_genre_methods():
     print("\nPress 'b' to go back Main Menu.")
-    print("Press 'e' to exit the program.")
+    print("Press 'e' to exit the program.")    
     print_line()
 
 #**********************************************************************************************
