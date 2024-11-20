24c985b42d50412713f77ab4e6e4d5a3726dc628
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2024-11-19 21:14:18 -0800

    Current status

--- lib/seed.py
+++ lib/seed.py
@@ -2,18 +2,21 @@ from models.__init__ import CONN, CURSOR
 from models.genre import Genre
 from models.band import Band
 from models.album import Album
-from lib.models.band_genre import BandGenre
+from models.band_genre import BandGenre
+from models.album_genre import AlbumGenre
 
 def seed_database():
     Genre.drop_table()
     Band.drop_table()
     Album.drop_table()
     BandGenre.drop_table()
+    AlbumGenre.drop_table()
 
     Genre.create_table()
     Band.create_table()
     Album.create_table()
     BandGenre.create_table()
+    AlbumGenre.create_table()
 
 seed_database()
 print("Seeded database")
